445cb27d78bb46858909d50289e140ebb13541ff
Yaroslav <yarmkhlv@gmail.com>
2023-10-10 11:10:23 +0300

    prep

---
+++ forms/src/app.jsx
@@ -0,0 +1,13 @@
+import { Form } from "./components/form/form";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="wrapper">
+        <Form></Form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
